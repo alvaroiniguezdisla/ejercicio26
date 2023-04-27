a8bac2b884e99136bb850c2abb861eeeeb14ec8f
Álvaro <alvaro.iniguezdisla@usp.ceu.es>
2023-04-27 15:03:39 +0200

    ej 26 terminado

--- src/main/java/org/example/ej2.java
+++ src/main/java/org/example/ej2.java
@@ -1,2 +1,162 @@
-package org.example;public class ej2 {
+package org.example;
+import java.util.ArrayList;
+
+public class ej2 {
+    public	static int suma(int n) {
+        if(n<=1) {
+            return n;
+        }else {
+            return n+ suma(n-1);
+        }
+    }
+
+    public static int fac(int n) {
+        if(n<=1) {
+            return 1;
+        }else {
+            return n*fac(n-1);
+        }
+    }
+
+    public static int  potencia(int  base,int exponente) {
+
+        if( exponente==0) {
+            return 1;
+
+        }else {
+            return (base)*potencia(base,exponente-1);
+
+        }
+    }
+
+    public static int sumalista( int[] lista,int i) {
+        if(i==0) {
+            return lista[0];
+        }else {
+            return lista[i]+sumalista(lista,i-1);
+
+
+        }
+    }
+
+    public static double media(int[] lista,int i) {
+        if(i==0) {
+            return lista[i];
+        }else {
+
+            return 	lista[i]+media(lista,i-1);
+        }
+
+    }
+
+
+    public static double cuadrados(int[]lista,int i) {
+        if(i==0) {
+            return lista[i]*lista[i];
+        }else {
+            return lista[i]*lista[i]+cuadrados(lista,i-1);
+        }
+    }
+    public static double desviacion(int[]lista) {
+        double media=(double)media(lista,lista.length-1)/lista.length;
+        double cuadrados=(double)cuadrados(lista,lista.length-1);
+        double varianza=(cuadrados/lista.length)-(media*media);
+        double desviacion=Math.sqrt(varianza);
+        return  desviacion;
+    }
+
+    public static int sumapares(int n) {
+
+        if(n%2!=0) {
+            n=n-1;
+        }
+        if(n==0) {
+            return n;
+        }else {
+            return n+sumapares(n-2);
+        }
+
+    }
+    public static ArrayList suma1 (int[]lista, ArrayList lista2, int n) {
+        if (n == 0) {
+            if (lista[0] % 2 == 0) {
+                lista2.add(lista[n]);
+                return lista2;
+            } else
+                return lista2;
+        } else {
+
+            if (lista[n] % 2 != 0)
+                return suma1(lista, lista2, n - 1);
+            else
+                lista2.add(lista[n]);
+            return suma1(lista, lista2, n - 1);
+        }
+    }
+
+
+    public static ArrayList pares (ArrayList lista3,int n){
+        if (n == 2) {
+            lista3.add(n);
+            return lista3;
+        } else {
+            if (n % 2 == 0) {
+                lista3.add(n);
+                return pares(lista3, n - 1);
+            } else {
+                return pares(lista3, n - 1);
+
+            }
+        }
+
+    }
+
+    public static int pescalar ( int[] lista1, int[] lista2, int i){
+        if (i < 0) {
+            return 0;
+        } else {
+            return (lista1[i] * lista2[i]) + pescalar(lista1, lista2, i - 1);
+        }
+    }
+
+    public static int fib ( int n){
+        if (n <= 1) {
+            return n;
+        } else {
+            return fib(n - 1) + fib(n - 2);
+        }
+
+    }
+
+    public static int fib13 ( int n){
+        if (n < 2) {
+            return n;
+        } else {
+            return fib13(n - 1) + fib13(n - 2);
+        }
+
+    }
+    public static int fib12 ( int n){
+        if (n < 2) {
+            return n;
+        } else {
+            return fib12(n - 1) + fib12(n - 2);
+        }
+
+    }
+
+    public static boolean equals ( double num){
+        double n13 = fib13(13);
+        double n12 = fib13(12);
+        double cociente = (double) (n13 / n12);
+        if (Math.abs(num - cociente) < 0.1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
 }
+
+
--- src/test/java/org/example/ej2Test.java
+++ src/test/java/org/example/ej2Test.java
@@ -1,4 +1,172 @@
+package org.example;
+
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.*;
+import org.junit.jupiter.api.Test;
+
 class ej2Test {
-  
+
+    @org.junit.jupiter.api.Test
+    void Testsumanumero0() {
+        assertEquals(0,ej2.suma(0));
+    }
+    @org.junit.jupiter.api.Test
+    void Testsumanumero5() {
+        assertEquals(15,ej2.suma(5));
+    }
+
+
+
+    @org.junit.jupiter.api.Test
+    void factorial0() {
+        assertEquals(1,ej2.fac(0));
+    }
+    @org.junit.jupiter.api.Test
+    void factorial3() {
+        assertEquals(6,ej2.fac(3));
+    }
+
+
+    @org.junit.jupiter.api.Test
+    void potencia3elevadoa0() {
+        assertEquals(1,ej2.potencia(3,0));
+    }
+    @org.junit.jupiter.api.Test
+    void potencia2elevadoa3() {
+        assertEquals(8,ej2.potencia(2,3));
+    }
+
+
+    @org.junit.jupiter.api.Test
+    void sumalistavacia() {
+        int[] lista = new int[0];
+        assertEquals(0,ej2.sumalista(lista,0));
+    }
+    @org.junit.jupiter.api.Test
+    void sumalista3elementos(){
+        int[] lista = {1,2,3};
+        assertEquals(6,ej2.sumalista(lista,2));
+    }
+
+    @org.junit.jupiter.api.Test
+    void medialista0elementos() {
+        int[] lista = new int[0];
+        assertEquals(0,ej2.media(lista,0));
+    }
+    @org.junit.jupiter.api.Test
+    void medialista3elementos() {
+        int[] lista = {1,2,3};
+        assertEquals(6,ej2.media(lista,2));
+    }
+
+
+
+    @org.junit.jupiter.api.Test
+    void cuadradoslista0elementos() {
+        int[] lista = new int[0];
+        assertEquals(0,ej2.cuadrados(lista,0));
+    }
+    @org.junit.jupiter.api.Test
+    void cuadradoslista3elementos() {
+        int[] lista = {1,2,3};
+        assertEquals(14,ej2.cuadrados(lista,2));
+    }
+
+    @org.junit.jupiter.api.Test
+    void desviacionlista0elementos() {
+        int[] lista = new int[0];
+        assertEquals(0,ej2.desviacion(lista));
+    }
+    @org.junit.jupiter.api.Test
+    void desviacionlista3elementos() {
+        int[] lista = {1,2,3};
+        assertEquals(0.816496580927726,ej2.desviacion(lista));
+    }
+    @org.junit.jupiter.api.Test
+    void sumaparesde0() {
+        assertEquals(   0,ej2.sumapares(0));
+    }
+    @org.junit.jupiter.api.Test
+    void sumaparesde5() {
+        assertEquals(6,ej2.sumapares(5));
+    }
+    @org.junit.jupiter.api.Test
+    void pares0() {
+       ArrayList lista3 = new ArrayList();
+       ArrayList lista2 = new ArrayList();
+
+        assertEquals(lista2,ej2.pares( lista3, 0));
+    }
+
+    @org.junit.jupiter.api.Test
+    void pares5(){
+        ArrayList lista2 = new ArrayList();
+        lista2.add(2);
+        lista2.add(4);
+        ArrayList lista3 = new ArrayList();
+
+        assertEquals(lista2,ej2.pares( lista3, 5));
+    }
+
+    @org.junit.jupiter.api.Test
+    void suma1_0elementos() {
+        int[] lista = {};
+        ArrayList lista2 = new ArrayList();
+        ArrayList lista3 = new ArrayList();
+
+        assertEquals(lista2,ej2.suma1(lista,lista3,lista.length-1));
+    }
+
+    @org.junit.jupiter.api.Test
+    void suma1_3elementos() {
+        int[] lista = {1,2,3};
+        ArrayList lista2 = new ArrayList();
+        lista2.add(2);
+        ArrayList lista3 = new ArrayList();
+
+        assertEquals(lista2,ej2.suma1(lista,lista3,lista.length-1));
+    }
+
+
+
+
+    @org.junit.jupiter.api.Test
+    void pescalar_0elementos() {
+        int[] lista = {};
+        int[] lista2 = {};
+        assertEquals(0,ej2.pescalar(lista,lista2,lista.length-1));
+
+    }
+    @org.junit.jupiter.api.Test
+    void pescalar_3elementos() {
+        int[] lista = {1,2,3};
+        int[] lista2 = {1,2,3};
+        assertEquals(14,ej2.pescalar(lista,lista2,lista.length-1));
+    }
+
+    @org.junit.jupiter.api.Test
+    void fib20() {
+        assertEquals(6765,ej2.fib(20));
+    }
+    @org.junit.jupiter.api.Test
+    void fib0(){
+        assertEquals(0,ej2.fib(0));
+    }
+
+    @org.junit.jupiter.api.Test
+    void fib13() {
+        assertEquals(233,ej2.fib(13));
+    }
+
+    @org.junit.jupiter.api.Test
+    void fib12() {
+        assertEquals(144,ej2.fib(12));
+    }
+
+    @org.junit.jupiter.api.Test
+    void testEquals() {
+        double num=(1+Math.sqrt(5))/2;
+        assertEquals(true,ej2.equals(num));
+
+    }
 }
